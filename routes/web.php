8cc7eaff1bcb5ba2c7a5cfc333d841bd5afda3fc
sspangsberg <smsj@easv.dk>
2021-02-19 13:44:27 +0100

    Added Auth facade to web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\ProductController;
+
 
 /*
 |--------------------------------------------------------------------------
