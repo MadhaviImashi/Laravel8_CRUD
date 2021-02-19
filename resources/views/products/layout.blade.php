bc86557f688ee35f8b5323e64cb3092bc4fc1fdc
sspangsberg <smsj@easv.dk>
2021-02-19 13:42:05 +0100

    Auth added and modified Blade files

--- resources/views/products/layout.blade.php
+++ resources/views/products/layout.blade.php
@@ -1,10 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Laravel 8 CRUD Application</title>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
-</head>
-<body>
+@extends('layouts.app')
 
 <div class="container">
     <br>
--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,13 @@ use App\Http\Controllers\ProductController;
 */
 
 
-Route::get('/', [ProductController::class, 'index']);
-Route::resource('products', ProductController::class);
+Route::resource('products', ProductController::class)->middleware('auth');
+
+Route::get('/', function() {
+    return view('home');
+});
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
